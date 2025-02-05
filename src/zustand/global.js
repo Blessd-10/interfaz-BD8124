c52205bce9d7ded7283ed0d7b47719a3ec1b800d
Andres Plaza (@bue221) <camiloplaza3@gmail.com>
2025-02-05 11:30:02 -0500

    Add selectedCategory state and setter to global Zustand store

--- src/zustand/global.js
+++ src/zustand/global.js
@@ -3,6 +3,8 @@ import { create } from "zustand";
 const useGlobalStore = create((set) => ({
   user: null,
   setUser: (user) => set({ user }),
+  selectedCategory: null,
+  setSelectedCategory: (selectedCategory) => set({ selectedCategory }),
 }));
 
 export default useGlobalStore;
